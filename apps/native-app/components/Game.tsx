48e74f66c605fb48051fd8e862b238fcf66e6b7a
luddebudde <ludwig.lindgr@gmail.com>
2024-08-22 23:02:36 +0200

    Added color indikation o nplayer lsit and almost made the map divided up with different areas whic hcities controlls

--- apps/native-app/components/Game.tsx
+++ apps/native-app/components/Game.tsx
@@ -1,4 +1,4 @@
-import { Client, Room } from 'colyseus.js'
+import { Client, Room } from "colyseus.js";
 import {
   Fragment,
   ReactNode,
@@ -6,50 +6,102 @@ import {
   useEffect,
   useRef,
   useState,
-} from 'react'
+} from "react";
 import {
   Canvas,
   GroupProps,
   MeshProps,
   ThreeEvent,
   Vector3,
-} from '@react-three/fiber/native'
+} from "@react-three/fiber/native";
 import {
   PerspectiveCamera,
   Plane,
   Sphere,
   Line,
-} from '@react-three/drei/native'
-import { closestTown, MyRoomState, Vec, ToJSON, origin } from 'server'
-import { Text, View, StyleSheet, Platform } from 'react-native'
-import { FpsCounter } from '@/components/Fps'
+} from "@react-three/drei/native";
+import { closestTown, MyRoomState, Vec, ToJSON, origin } from "server";
+import { Text, View, StyleSheet, Platform } from "react-native";
+import { FpsCounter } from "@/components/Fps";
+import { ShaderMaterial } from "three";
 
-export const vec3 = (v: Vec) => [v.x, 0, v.y] as [number, number, number]
-export const origin3: Vector3 = [0, 0, 0]
+const defaultVert = `
+varying vec3 vUv; 
+
+void main() {
+  vUv = position; 
+
+  vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
+  gl_Position = projectionMatrix * modelViewPosition; 
+}
+`;
+
+const defaultFrag = `
+uniform vec3 colorA; 
+uniform vec3 colorB; 
+varying vec3 vUv;
+
+void main() {
+  gl_FragColor = vec4(mix(colorA, colorB, vUv.z), 1.0);
+}
+`;
+
+const defaultGroundUniform = {
+  towns: {
+    value: [
+      // xy
+      0.0, 0.0,
+      // rgb
+      0.0, 0.5, 0.0,
+      //  xy
+      10.0, 10.0,
+      // rgb
+      0.5, 0.5, 0.0,
+    ],
+  },
+};
+
+const groundFrag = `
+const int maxCities = 50;
+const int len = (2 + 3) * maxCities;
+uniform float towns[len]; 
+varying vec3 vUv;
+
+void main() {
+  int closestTownIndex = 0;
+  float closestDistance = 1e100;
+  for (int i = 0; i < 2 ; i++){
+    // vec2 closestTown = vec2(towns[closestTownIndex * 5 + 0], towns[closestTownIndex * 5 + 0 + 1]);
+    vec2 currentTown = vec2(towns[i * 5 + 0], towns[i * 5 + 0 + 1]);
+    float d = distance(vUv.xy, currentTown);
+    if (d < closestDistance){
+      closestTownIndex = i;
+      closestDistance = d;
+    }
+  }
+  //   
+  vec3 color = vec3(towns[closestTownIndex * 5 + 2 + 0], towns[closestTownIndex * 5 + 2 + 1], towns[closestTownIndex * 5 + 2 + 2]);
+  gl_FragColor = vec4(color, 1.0);
+}
+`;
+
+export const vec3 = (v: Vec) => [v.x, 0, v.y] as [number, number, number];
+export const origin3: Vector3 = [0, 0, 0];
 
 const PlayerMesh = (props: MeshProps & { color: string }) => (
-  <mesh
-    scale={0.15}
-    castShadow
-    receiveShadow
-    {...props}
-  >
+  <mesh scale={0.15} castShadow receiveShadow {...props}>
     <cylinderGeometry args={[1, 1, 1]} />
     <meshStandardMaterial color={props.color} />
   </mesh>
-)
+);
 
-type TownMeshProps = GroupProps & { label: ReactNode }
+type TownMeshProps = GroupProps & { label: ReactNode };
 
 const TownMesh = (props: TownMeshProps) => (
   <group {...props}>
-    <mesh
-      scale={[0.1, 0.2, 0.1]}
-      castShadow
-      receiveShadow
-    >
+    <mesh scale={[0.1, 0.2, 0.1]} castShadow receiveShadow>
       <cylinderGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={'#000000'} />
+      <meshStandardMaterial color={"#000000"} />
     </mesh>
     {/* TODO fix */}
     {/*<Html*/}
@@ -59,57 +111,52 @@ const TownMesh = (props: TownMeshProps) => (
     {/*  {props.label}*/}
     {/*</Html>*/}
   </group>
-)
+);
 
 const TargetMesh = (props: MeshProps) => (
-  <mesh
-    scale={[0.03, 1, 0.03]}
-    castShadow
-    receiveShadow
-    {...props}
-  >
+  <mesh scale={[0.03, 1, 0.03]} castShadow receiveShadow {...props}>
     <cylinderGeometry args={[1, 1, 1]} />
-    <meshStandardMaterial color={'red'} />
+    <meshStandardMaterial color={"red"} />
   </mesh>
-)
+);
 
 type GameState =
   | {
-      tag: 'loading'
+      tag: "loading";
     }
   | {
-      tag: 'loaded'
-      room: Room
-      state: ToJSON<MyRoomState>
-    }
+      tag: "loaded";
+      room: Room;
+      state: ToJSON<MyRoomState>;
+    };
 
 const useGame = (): GameState => {
   const [state, setState] = useState<ToJSON<MyRoomState>>({
     players: {},
     towns: {},
     size: { width: 0, height: 0 },
-  })
+  });
 
-  const roomRef = useRef<Room>()
+  const roomRef = useRef<Room>();
 
   useEffect(() => {
     const join = async () => {
-      const port = '2567'
-      const ip = Platform.OS === 'android' ? '192.168.129.143' : 'localhost'
-      const client = new Client(`ws://${ip}:${port}`)
-      console.log('Joining room...')
+      const port = "2567";
+      const ip = Platform.OS === "android" ? "192.168.129.143" : "localhost";
+      const client = new Client(`ws://${ip}:${port}`);
+      console.log("Joining room...");
       try {
-        roomRef.current = await client.joinOrCreate<MyRoomState>('my_room')
-        console.log('Joined successfully!')
+        roomRef.current = await client.joinOrCreate<MyRoomState>("my_room");
+        console.log("Joined successfully!");
 
-        const room = roomRef.current
+        const room = roomRef.current;
         // Initialize state
         room.onStateChange.once((state) => {
-          setState(() => room.state.toJSON())
-        })
+          setState(() => room.state.toJSON());
+        });
 
         // Subscribe to state changes
-        const watchedEntityTypes = ['players', 'towns'] as const
+        const watchedEntityTypes = ["players", "towns"] as const;
         watchedEntityTypes.forEach((entityType) => {
           room.state[entityType].onChange((entity, id) => {
             const updateState = () =>
@@ -119,36 +166,48 @@ const useGame = (): GameState => {
                   ...state[entityType],
                   [id]: entity.toJSON(),
                 },
-              }))
-            entity.onChange(updateState)
-          })
-        })
+              }));
+            entity.onChange(updateState);
+          });
+        });
       } catch (e) {
-        console.error(e)
+        console.error(e);
       }
-    }
-    join()
+    };
+    join();
     const handle = setInterval(() => {
       // Update
-      const room = roomRef.current
+      const room = roomRef.current;
       if (!room) {
-        return
+        return;
       }
       // room.send(0, inputPayload)
-    }, 100)
-    return () => clearInterval(handle)
-  }, [])
+    }, 100);
+    return () => clearInterval(handle);
+  }, []);
+
+  useEffect(() => {
+    groundMaterial.uniforms.towns.value = Object.values(state.towns)
+      .map((town) => [town.pos.x, town.pos.y, 1.0, 0.0, 0.0])
+      .flat();
+  }, [state.towns]);
 
   return !roomRef.current
     ? {
-        tag: 'loading',
+        tag: "loading",
       }
     : {
-        tag: 'loaded',
+        tag: "loaded",
         room: roomRef.current,
         state,
-      }
-}
+      };
+};
+
+const groundMaterial = new ShaderMaterial({
+  uniforms: defaultGroundUniform,
+  vertexShader: defaultVert,
+  fragmentShader: groundFrag,
+});
 
 const styles = StyleSheet.create({
   game: {
@@ -159,77 +218,80 @@ const styles = StyleSheet.create({
   },
   header: {
     fontSize: 20,
-    fontWeight: 'bold',
-    alignItems: 'center',
+    fontWeight: "bold",
+    alignItems: "center",
     gap: 10,
   },
   ul: {
-    flexDirection: 'column',
-    alignItems: 'flex-start',
+    flexDirection: "column",
+    alignItems: "flex-start",
   },
   li: {
-    flexDirection: 'row',
-    alignItems: 'flex-start',
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 5,
   },
   liBullet: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 24,
   },
   liText: {
     fontSize: 18,
   },
   townLabel: {
-    color: 'white',
-    flexWrap: 'nowrap',
+    color: "white",
+    flexWrap: "nowrap",
   },
-})
+});
 
 export const Game = () => {
-  const state = useGame()
+  const state = useGame();
 
-  if (state.tag === 'loading') {
-    return <Text>Loading...</Text>
+  if (state.tag === "loading") {
+    return <Text>Loading...</Text>;
   }
 
-  return <LoadedGame state={state} />
-}
+  return <LoadedGame state={state} />;
+};
 
 const LoadedGame = (props: {
   state: Extract<
     GameState,
     {
-      tag: 'loaded'
+      tag: "loaded";
     }
-  >
+  >;
 }) => {
-  const s = props.state
+  const s = props.state;
 
-  const { state, room } = s
+  const { state, room } = s;
 
   const handleClick = useCallback(
     (event: ThreeEvent<MouseEvent>) => {
-      console.log('sending click')
-      room.send('click', {
+      console.log("sending click");
+      room.send("click", {
         x: event.point.x,
         y: event.point.z,
-      })
+      });
     },
     [room],
-  )
+  );
 
-  const me = state.players[room.sessionId ?? '']
+  const me = state.players[room.sessionId ?? ""];
 
   if (!me) {
-    return <Text>Loading...</Text>
+    return <Text>Loading...</Text>;
   }
 
-  const towns = Object.values(state.towns)
+  const towns = Object.values(state.towns);
 
   return (
     <View style={styles.game}>
       <View
         style={{
-          position: 'absolute',
+          position: "absolute",
           zIndex: 1,
           padding: 10,
         }}
@@ -248,20 +310,22 @@ const LoadedGame = (props: {
           <Text style={styles.header}>Players:</Text>
           <View style={styles.ul}>
             {Object.entries(state.players).map(([playerId, player]) => (
-              <Text
-                key={playerId}
-                style={styles.li}
-              >
-                {playerId} ({player.color})
-              </Text>
+              <View key={playerId} style={styles.li}>
+                <View
+                  style={{
+                    backgroundColor: player.color,
+                    width: 20,
+                    height: 20,
+                    borderRadius: 10,
+                  }}
+                ></View>
+                <Text>{playerId}</Text>
+              </View>
             ))}
           </View>
         </View>
       </View>
-      <Canvas
-        style={styles.canvas}
-        shadows
-      >
+      <Canvas style={styles.canvas} shadows>
         <ambientLight intensity={1.5} />
         <Sphere position={[-5, 5, 5]}>
           <meshStandardMaterial color="yellow" />
@@ -272,17 +336,14 @@ const LoadedGame = (props: {
           shadow-mapSize={[1024, 1024]}
           castShadow
         ></directionalLight>
-        <PerspectiveCamera
-          position={[2, 2, 10]}
-          makeDefault
-        />
+        <PerspectiveCamera position={[2, 2, 10]} makeDefault />
         <Plane
           args={[state.size.width, state.size.height]}
           rotation={[-Math.PI / 2, 0, 0]}
           receiveShadow
           onClick={handleClick}
         >
-          <meshStandardMaterial color="green" />
+          <shaderMaterial attach="material" args={[groundMaterial]} />
         </Plane>
         {Object.entries(state.players).map(([playerId, player]) => (
           <Fragment key={playerId}>
@@ -311,7 +372,7 @@ const LoadedGame = (props: {
               <View>
                 <Text style={styles.townLabel}>{town.name}</Text>
                 <Text style={styles.townLabel}>
-                  🍏{' '}
+                  🍏{" "}
                   {Math.ceil(
                     (town.dailyRations / (365 * town.population)) * 100,
                   )}
@@ -325,5 +386,5 @@ const LoadedGame = (props: {
         ))}
       </Canvas>
     </View>
-  )
-}
+  );
+};
